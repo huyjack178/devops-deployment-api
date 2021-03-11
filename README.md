fb5a4b04d7a59b76e5a5e1d40dc129e06e395b8d
Harrison Nguyen <harrison.nguyen@luminetsoft.com>
2021-03-11 11:29:24 +0700

    update

---
+++ README.md
@@ -0,0 +1,3 @@
+```
+curl -v -F host=10.10.50.18 -F port=22 -F user=root -F remotePath=/var/www -F file=@"/Users/harrison.nguyen/Downloads/haproxy (1).cfg" http://localhost:22222/scp
+```
--- src/server.js
+++ src/server.js
@@ -1,15 +1,23 @@
 const { NodeSSH } = require('node-ssh');
+const fs = require('fs');
 const ssh = new NodeSSH();
-const express = require('express');
-const app = express();
+const fastify = require('fastify');
+const path = require('path');
+const multer = require('fastify-multer');
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
 const port = 22222;
+const server = fastify();
 
-app.use(express.json());
-app.get('/', (req, res) => {
+server.register(multer.contentParser).register(require('fastify-cors'), {
+  origin: '*',
+});
+
+server.get('/', (req, res) => {
   res.send('Deployment API is running!');
 });
 
-app.post('/ssh', (req, res) => {
+server.post('/command', (req, res) => {
   console.log(req.body);
 
   ssh
@@ -17,7 +25,7 @@ app.post('/ssh', (req, res) => {
       host: req.body.host,
       port: req.body.port ?? 22,
       username: req.body.user ?? root,
-      privateKey: './id_rsa',
+      privateKey: path.join(__dirname, 'id_rsa'),
     })
     .then(() => {
       ssh
@@ -34,6 +42,67 @@ app.post('/ssh', (req, res) => {
     .catch((error) => console.log(error));
 });
 
-app.listen(port, '0.0.0.0', () => {
-  console.log(`app listening at http://0.0.0.0:${port}`);
+server.post('/scp', { preHandler: upload.single('file') }, async (req, res) => {
+  console.log(req.file);
+  const file = req.file;
+  const fileName = file.originalname;
+  const folderPath = mkDirByPathSync('./temp/' + Math.floor(new Date().getTime() / 1000), true);
+  fs.writeFileSync(`${folderPath}/${fileName}`, file.buffer);
+
+  ssh
+    .connect({
+      host: req.body.host,
+      port: req.body.port ?? 22,
+      username: req.body.user ?? root,
+      privateKey: path.join(__dirname, 'id_rsa'),
+    })
+    .then(() => {
+      ssh
+        .putFile(`${folderPath}/${fileName}`, `${req.body.remotePath}/${fileName}`)
+        .then(() => {
+          fs.rmdirSync(folderPath, { recursive: true });
+          res.status(200).send('success');
+        })
+        .catch((error) => console.log(error));
+    })
+    .catch((error) => console.log(error));
+});
+
+const mkDirByPathSync = (targetDir, { isRelativeToScript = false } = {}) => {
+  const sep = path.sep;
+  const initDir = path.isAbsolute(targetDir) ? sep : '';
+  const baseDir = isRelativeToScript ? __dirname : '.';
+
+  return targetDir.split(sep).reduce((parentDir, childDir) => {
+    const curDir = path.resolve(baseDir, parentDir, childDir);
+    try {
+      fs.mkdirSync(curDir, { recursive: true });
+    } catch (err) {
+      if (err.code === 'EEXIST') {
+        return curDir;
+      }
+
+      // To avoid `EISDIR` error on Mac and `EACCES`-->`ENOENT` and `EPERM` on Windows.
+      if (err.code === 'ENOENT') {
+        // Throw the original parentDir error on curDir `ENOENT` failure.
+        throw new Error(`EACCES: permission denied, mkdir '${parentDir}'`);
+      }
+
+      const caughtErr = ['EACCES', 'EPERM', 'EISDIR'].indexOf(err.code) > -1;
+      if (!caughtErr || (caughtErr && curDir === path.resolve(targetDir))) {
+        throw err; // Throw if it's just the last created dir.
+      }
+    }
+
+    return curDir;
+  }, initDir);
+};
+
+server.listen(port, '0.0.0.0', (err, address) => {
+  if (err) {
+    console.error(err);
+    process.exit(1);
+  }
+
+  console.log(`Server listening at ${address}`);
 });
