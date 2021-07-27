cf82cf76df9fbdc61adcf3d2b74ae45dfbd3c864
Harrison Nguyen <harrison.nguyen@luminetsoft.com>
2021-07-27 15:22:50 +0700

    update

---
+++ src/configs.KBT.js
@@ -0,0 +1,3 @@
+module.exports = {
+  sentryDsn: 'https://d42a3908f4a14147a0e89c3bac3f515b@10.99.99.51:801/167',
+};
--- src/configs.UAT.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  sentryDsn: 'http://f55e0371ad2544e99563e2f9f24d59a8@10.99.99.187:803/155',
-};
